05c547458fe1ad3f5c706f0d86260ec122f0c042
ViniciusLeite13 <70172639+ViniciusLeite13@users.noreply.github.com>
2021-08-05 21:06:35 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <DS3232RTC.h> // inclusao da biblioteca para o modulo timer
+#include <stdio.h>
+#include <time.h>
+#include <Streaming.h>
+#include "funcoes.h"
+
+void main (){
+int x = 0;
+File *log;
+
+while ( x < 1){
+  void time();
+ 
+  t = now();
+  if (t != tLast) {
+    tLast = t;
+    printDateTime(t);
+    
+    //Grava dados no cartao a cada 1 minuto
+    if (second(t) == 0)
+    {
+      grava_SD(t);
+      LCDshow();
+    }
+  }
+}
+}
